b1a7a4e7ad7a7b3551e6ea001e6322b9dc2e4d25
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-05 14:08:07 +0530

    Added:Game play is added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,20 +5,51 @@ import java.util.Random;
 public class SnakeAndLadder
 {
 	//uc1 variables initialized at starting positions
-	static final int STARTINGPOSITION=0;
-	static final int PLAYER=1;
-	static Random RANDOM = new Random();
+	static  int STARTINGPOSITION=0;
+	
+	
+	static  int PLAYER=1;
+	static Random random= new Random();
+	static final int LADDER = 1;
+	static final int SNAKE = 2;
+	static final int NOPLAY = 0;
    
-//uc2 rolling a random value to the player
-public void diceRoll()
-{
-	int DICE = (RANDOM.nextInt(6)+1);
-	System.out.println("number generated " +DICE);
-}
-public static void main(String[]args)
-{
-	 SnakeAndLadder s=new SnakeAndLadder();
-	 s.diceRoll();
-}
+    //uc2 rolling a random value to the player
+    public int diceRoll(){
+	int dice = (random.nextInt(6)+1);
+	System.out.println("number generated " +dice);
+	return dice;
+    }
+    
+    public void gamePlay() {
+    	int playerPosition=STARTINGPOSITION;
+    	System.out.println("current player position:"+playerPosition); 
+    	int dice=diceRoll();
+    	int action = random.nextInt(3);
+    	System.out.println("action Invoked:"+action); 
+		switch(action)
+		{
+			case LADDER:
+				 playerPosition = playerPosition + dice;
+				 System.out.println("current player position:"+playerPosition); 
+			     break;
+			case SNAKE:
+				playerPosition = playerPosition - dice;
+				System.out.println("current player position:"+playerPosition); 
+			    break;
+			case NOPLAY:
+				playerPosition = playerPosition;
+				System.out.println("current player position"+playerPosition);
+				break;
+		}
+		
+    	
+    }
+
+    public static void main(String[]args){
+	 SnakeAndLadder snakeAndLadder=new SnakeAndLadder();
+	 snakeAndLadder.gamePlay();
+	 }
+  
 }
 
