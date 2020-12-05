68e5d7f008a09fe405580542c5ac46503a78d594
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-05 11:51:13 +0530

    Added:Dice rolled for the player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,24 @@
 package com.SnakeAndLadder.java;
 
-public class SnakeAndLadder {
+import java.util.Random;
+
+public class SnakeAndLadder
+{
 	//uc1 variables initialized at starting positions
 	static final int STARTINGPOSITION=0;
-
-
-	
+	static final int PLAYER=1;
+	static Random RANDOM = new Random();
+   
+//uc2 rolling a random value to the player
+public void diceRoll()
+{
+	int DICE = (RANDOM.nextInt(6)+1);
+	System.out.println("number generated " +DICE);
 }
+public static void main(String[]args)
+{
+	 SnakeAndLadder s=new SnakeAndLadder();
+	 s.diceRoll();
+}
+}
+
